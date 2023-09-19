7125396b74058f9aa601954761ca23195e26a3ff
semicolon-id <ramabagas872@gmail.com>
2023-09-19 20:45:40 +0700

    Add NotFound

--- goodfood/src/pages/NotFound.jsx
+++ goodfood/src/pages/NotFound.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function NotFound() {
   return (
     <div className="text-center text-[#1F384C] font-semibold font-sans mx-auto py-32">
-      <h1 className="text-6xl font-semibold font-sans">Page Not Found</h1>
+      <h1 className="text-6xl font-semibold font-sans">Error: Page Not Found</h1>
     </div>
   );
 }
